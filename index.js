02ff339275458c392849d75ea8c806e6f867a37a
digitaltsai <digitaltsai@gmail.com>
2016-11-25 11:35:41 -0800

    Removed request dependency

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 var nock   = require('nock');
-var request = require('request');
+var http = require('http');
 
+// comment out the below nock portion, and the timeout will work
 nock('http://google.com')
   .get('/')
   .delay(1000)
   .reply(200, {});
+// comment out above block to test without nock
 
-request({
-  url: 'http://google.com',
-  timeout: 10
-}, function (err, response) {
-  console.log(err); // should be expecting ESOCKETTIMEDOUT, but prints null
+var req = http.get({
+  host: 'google.com',
+  path: '/'
+}, function(res) {
+  var body = '';
+  res.on('data', function(data) {
+    body += data;
+  });
+
+  res.on('end', function() {
+    console.log('ended, supposed to show "timed out"');
+  });
+}).on('error', function() {
+  console.log('errored (probably through timeout for this test)');
+});
+
+// https://nodejs.org/api/http.html#http_request_settimeout_timeout_callback
+req.setTimeout(1, function() {
+  console.log('timed out');
+  req.abort();
 });
